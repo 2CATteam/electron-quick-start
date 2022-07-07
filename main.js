202612d40bc4c741784006613da9f587ae5e0ab5
Daniel Royer <2CATteam@gmail.com>
2022-07-06 22:06:28 -0500

    I think it should work now

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    autoHideMenuBar: true
   })
 
   // and load the index.html of the app.
---
+++ scripts.js
@@ -0,0 +1,91 @@
+let fs = require('fs')
+
+let songs = {}
+
+function save() {
+    fs.writeFileSync("config.json", JSON.stringify(songs, null, "\t"), "utf-8")
+}
+
+function stopAll() {
+    let sources = document.querySelectorAll("audio")
+    for (let i = 0; i < sources.length; i++) {
+        sources[i].pause()
+        sources[i].currentTime = 0
+    }
+}
+
+function loadSongs() {
+    let files = fs.readdirSync("./")
+    for (let i in files) {
+        if (files[i].match(/.(wav|mp3|ogg)$/)) {
+            songs[files[i]] = {}
+        }
+    }
+    if (fs.existsSync("config.json")) {
+        let config = require("./config.json")
+        for (let i in config) {
+            if (songs[i]) {
+                songs[i].start = config[i].start
+                songs[i].end = config[i].end
+            }
+        }
+    }
+    generateRows()
+}
+
+function generateRows() {
+    for (let i in songs) {
+        let newElementText = `<tr>
+            <td>${i}</td>
+            <td><button class="startSong">Start song looping</button><td>
+            <td><button class="endSong">Let song end</button></td>
+            <td><label>Loop start (seconds): <input type="number" class="loopStart" ${songs[i].start ? 'value="' + songs[i].start + '"' : ""}></label></td>
+            <td><label>Loop end (seconds): <input type="number" class="loopEnd" ${songs[i].end ? 'value="' + songs[i].end + '"' : ""}></label></td>
+            <td><audio controls><source src="${i}" type="audio/${i.match(/.(wav|mp3|ogg)$/)[1]}"></audio></td>
+        </tr>`
+
+        //https://grrr.tech/posts/create-dom-node-from-html-string/
+        let placeholder = document.createElement("table")
+        placeholder.innerHTML = newElementText
+        let newElement = placeholder.firstElementChild
+
+        console.log(newElement)
+
+        newElement.querySelector(".loopStart").onchange = ((key, event) => {
+            songs[key].start = event.target.value
+        }).bind(null, i)
+        newElement.querySelector(".loopEnd").onchange = ((key, event) => {
+            songs[key].end = event.target.value
+        }).bind(null, i)
+        newElement.querySelector(".startSong").onclick = startSongLoop.bind(null, i)
+        newElement.querySelector(".endSong").onclick = letSongEnd.bind(null, i)
+
+        document.getElementById("songItems").append(newElement)
+        songs[i].element = newElement
+    }
+}
+
+function startSongLoop(key) {
+    songs[key].looping = true
+    songs[key].element.querySelector("audio").currentTime = 0
+    songs[key].element.querySelector("audio").play()
+}
+
+function letSongEnd(key) {
+    songs[key].looping = false
+}
+
+function loopSongs() {
+    for (let i in songs) {
+        if (songs[i].looping) {
+            if (songs[i].element.querySelector("audio").currentTime >= songs[i].end) {
+                songs[i].element.querySelector("audio").currentTime -= songs[i].end - songs[i].start
+            }
+        }
+    }
+}
+
+loadSongs()
+setInterval(loopSongs, 1)
+document.getElementById("saveButton").onclick = () => {save()}
+document.getElementById("stopButton").onclick = () => {stopAll()}
